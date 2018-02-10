27c2eb001940fe0b074a523ddccee2ceae1b498e
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2018-02-10 14:05:15 +0100

    fmt

--- src/Othello.hs
+++ src/Othello.hs
@@ -38,7 +38,7 @@ updateState e s = case e of
     if turn s == White then (s { turn = Black }, Nil) else (s, TurnError)
   (Turn White) ->
     if turn s == Black then (s { turn = White }, Nil) else (s, TurnError)
-  (Move' l p) -> if validMove (Move l p) (board s)
+  (Move' l p)  -> if validMove (Move l p) (board s)
     then (s { board = board' }, Nil)
     else (s, MoveError)
     where board' = updateBoard (Move l p) (board s)
@@ -50,10 +50,10 @@ searchDirections =
 validMove :: Move -> Board -> Bool
 validMove move board =
   let
-    (Move location player) = move
-    searchCutoff           = fromIntegral $ length board
-    paths = map (walkAndCollect searchCutoff location board) searchDirections
-    validPaths             = map (validMovePath player) paths
+    Move location player = move
+    searchCutoff         = fromIntegral $ length board
+    paths                = map (walkAndCollect searchCutoff location board) searchDirections
+    validPaths           = map (validMovePath player) paths
   in
     freePosition location board && or validPaths
 
@@ -67,7 +67,7 @@ onBoard (x, y) board = x >= 1 && x <= bound && y >= 1 && y <= bound
 freePosition :: Location -> Board -> Bool
 freePosition l b = case getSquareByLocation l b of
   Just (Square _ Nothing) -> True
-  _ -> False
+  _                       -> False
 
 negPlayer :: Player -> Player
 negPlayer Black = White
@@ -75,9 +75,9 @@ negPlayer White = Black
 
 -- This provides the actual valid move definition (2 pieces surrounding only enemy pieces)
 validMovePath :: Player -> [Square] -> Bool
-validMovePath p [] = False
+validMovePath p []  = False
 validMovePath p [_] = False
-validMovePath p ss =
+validMovePath p ss  =
   let negPlayerSquare        = Square (0, 0) (Just (negPlayer p))
       xs                     = reverse ss
       enemiesBetween         = map (squarePlayerEq negPlayerSquare) (tail xs) -- tail is inbetween
@@ -99,32 +99,30 @@ scaleLocation n (x, y) = (x * n, y * n)
 
 walkAndCollect :: Integer -> Location -> Board -> Direction -> [Square]
 walkAndCollect cutoff loc board dir =
-  let flatBoard = concat board
-      (x, y)    = dir
-      locationsToWalk =
-        [ addLocation loc (scaleLocation n dir) | n <- [1 .. cutoff] ]
-      squares = map (`getSquareByLocation` board) locationsToWalk
+  let flatBoard       = concat board
+      (x, y)          = dir
+      locationsToWalk = [ addLocation loc (scaleLocation n dir) | n <- [1 .. cutoff] ]
+      squares         = map (`getSquareByLocation` board) locationsToWalk
   in  catMaybes squares
 
 updateBoard :: Move -> Board -> Board
-updateBoard move board = 
+updateBoard move board =
   let
-    (Move location player) = move
-    searchCutoff           = fromIntegral $ length board
-    paths = map (walkAndCollect searchCutoff location board) searchDirections
-    squaresToPlace = concatMap (\p -> if validMovePath player p then p else []) paths
-    -- Include the piece being placed
-    squaresToPlace' = (Square location (Just player)) : squaresToPlace
-    squares = map (setColor player) squaresToPlace'
+    Move location player = move
+    searchCutoff         = fromIntegral $ length board
+    paths                = map (walkAndCollect searchCutoff location board) searchDirections
+    squaresToPlace       = concatMap (\p -> if validMovePath player p then p else []) paths
+    squaresToPlace'      = Square location (Just player) : squaresToPlace -- Include the piece being placed
+    squares              = map (setColor player) squaresToPlace'
   in
     placeSquares squares board
 
 placeSquares :: [Square] -> Board -> Board
-placeSquares ss board =
-  (map . map) (replaceSquare ss) board
+placeSquares ss =
+  (map . map) (replaceSquare ss)
     where
       replaceSquare [] s = s
-      replaceSquare ((Square loc' p'):ss) (Square loc p) =
+      replaceSquare (Square loc' p' : ss) (Square loc p) =
         if loc == loc'
           then Square loc' p'
           else replaceSquare ss (Square loc p)
