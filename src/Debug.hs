84a12724dcb3d8298780dcba4e3cff66e55e09ad
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2018-02-10 13:45:42 +0100

    removed debug function from Othello module

---
+++ src/Debug.hs
@@ -0,0 +1,29 @@
+module Debug where
+
+import Othello
+
+debugPrintBoard''' :: IO ()
+debugPrintBoard''' = putStrLn $ showBoard $ initialBoard 8
+
+debugBoard =
+  [ [Square (1, 1) (Just Black), Square (2, 1) Nothing]
+  , [Square (1, 2) Nothing, Square (2, 2) (Just White)]
+  ]
+
+debugBoard' =
+  [ [Square (1, 1) (Just White), Square (2, 1) (Just Black), Square (3, 1) Nothing]
+  , [Square (1, 2) Nothing, Square (2, 2) (Just Black), Square (3, 2) (Just Black)]
+  , [Square (1, 3) (Just White), Square (2, 3) (Just White), Square (3, 3) (Just White)]
+  ]
+
+debugValidMove = validMove (Move (0, 0) White) debugBoard
+debugValidMove' = validMove (Move (3, 1) White) debugBoard'
+
+debugState = State { turn = White, board = debugBoard}
+
+-- Should not work, out side of board
+debugEvent = updateState (Move' (0,0) White) debugState
+
+-- Should work, flipping multiple rows and diagonals
+debugEvent' = updateState (Move' (3,1) White) (debugState { board = debugBoard' })
+
